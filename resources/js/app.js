f2d4d8eeb3d2bf2b744ff1f253015fc731712b27
towin <cristian3211998@gmail.com>
2022-04-01 17:35:45 -0500

    Se crea vista login

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,47 @@
-require('./bootstrap');
+require("./bootstrap");
+
+// Import libraries
+window.Vue = require("vue").default;
+import vuetify from "./vuetify";
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+import router from "./routes";
+import App from "./components/App";
+
+Vue.use(VueSweetalert2);
+
+function loggedIn() {
+    return localStorage.getItem("token_Historial_Clinico_Oftamologico");
+}
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        if (!loggedIn()) {
+            next({
+            path: '/',
+            query: { redirect: to.fullPath }
+            })
+        } else {
+            next()
+        }
+    } else if(to.matched.some(record => record.meta.guest)) {
+        if (loggedIn()) {
+            next({
+            path: '/inicio/',
+            query: { redirect: to.fullPath }
+            })
+        } else {
+            next()
+        }
+    } else {
+        next()
+    }
+})
+
+const app = new Vue({
+    el: "#app",
+    router: router,
+    vuetify,
+    render(h) {
+        return h(App);
+    },
+});
---
+++ resources/js/components/errors/404.vue
@@ -0,0 +1,11 @@
+<template>
+<div>
+    <h1>error 404</h1>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
